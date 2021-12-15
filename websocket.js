67a3220cad3d32e5be0eb445c3cd38df7a817186
mdreame <1067774901@qq.com>
2021-12-16 00:14:33 +0800

    fixed: lost content after comma, cause two websocket server handler need to be change. Now fixed.

--- websocket.js
+++ websocket.js
@@ -13,8 +13,6 @@ router.ws("/jiang", (ws) => {
     });
     
     ws.on("message", (msg) => {
-        console.log("服务器接收："+msg);
-        
         jiangArr.push(msg);     //接收yun的消息
         ws.send("right" + msg);    //回流自己发送的消息
     });
@@ -23,7 +21,7 @@ router.ws("/jiang", (ws) => {
         if (yunArr.length > 0) {
             let msgItem = yunArr[0];
             yunArr.shift();
-            ws.send("left " + msgItem);
+            ws.send("left-" + msgItem);
         }
     }, 1000);
 });
@@ -39,7 +37,7 @@ router.ws("/yun", (ws) => {
         yunArr.push(msg);
 
         //自己发送的消息回流
-        ws.send("right," + msg);
+        ws.send("right" + msg);
     });
 
     //每秒钟检查数组有无新消息
@@ -47,7 +45,7 @@ router.ws("/yun", (ws) => {
         if (jiangArr.length > 0) {
             let msgItem = jiangArr[0];
             jiangArr.shift();
-            ws.send("left," + msgItem);
+            ws.send("left-" + msgItem);
         }
     }, 1000);
 });
